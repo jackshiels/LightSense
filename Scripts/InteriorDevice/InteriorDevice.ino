9c9970b2e387421a9e98852f9f437bf79d4c7daf
jack <jack.shiels.123@gmail.com>
2022-12-01 18:56:05 +0000

    Added the PIR capable interior script

---
+++ Scripts/InteriorDevice/InteriorDevice.ino
@@ -0,0 +1,21 @@
+// PIR sensor variables
+int PIRPin = D0;
+int sensorVal = 0;
+
+void setup() {
+  // Set up the PIR
+  pinMode(PIRPin, INPUT);
+  Serial.begin(115200);
+}
+
+void loop() {
+  delay(100);
+  sensorVal = digitalRead(PIRPin);
+  if (sensorVal == HIGH
+  && prevSensorVal != HIGH){
+    Serial.println("1");
+  }
+  else{
+    Serial.println("0");
+  }
+}
