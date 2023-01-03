4dc3ab4b35f3188bde60e356d6767d051af8e306
jack <jack.shiels.123@gmail.com>
2023-01-03 21:48:11 +0000

    Removed secrets.

--- Scripts/InteriorDevice/arduino_secrets.h
+++
@@ -1,5 +0,0 @@
-#define SECRET_SSID "5GEE-Router-893N";
-#define SECRET_PASS "Shellfish678*%*";
-#define MQTT_SERVER "stud-pi-ucfnhie.local";
-#define MQTT_USER "jacks";
-#define MQTT_PASS "Alabaster10&";
